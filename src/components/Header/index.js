b94f75738352c671a43b03931e6a499ce8682db4
David T. John <davidjohn_atl@yahoo.com>
2020-10-03 09:09:43 -0400

    Added Fontawesome icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faSignInAlt,
+  faBook,
+  faFileAlt,
+} from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
   return (
     <header>
@@ -10,13 +17,19 @@ const Header = () => {
       <nav>
         <ul id="nav-list">
           <li className="nav-item">
-            <Link to="/portfolio">Portfolio</Link>
+            <Link to="/portfolio">
+              <FontAwesomeIcon className="fa-icon" icon={faFileAlt} /> Portfolio
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/blog">Blog</Link>
+            <Link to="/blog">
+              <FontAwesomeIcon className="fa-icon" icon={faCoffee} /> Blog
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/login">Login</Link>
+            <Link to="/login">
+              <FontAwesomeIcon className="fa-icon" icon={faSignInAlt} /> Login
+            </Link>
           </li>
         </ul>
       </nav>
