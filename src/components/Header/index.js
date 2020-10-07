049084e1247f0cd61205d820e6e2f9de41d38f71
David T. John <davidjohn_atl@yahoo.com>
2020-10-06 20:05:29 -0400

    Added support for dark mode

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCoffee,
   faSignInAlt,
-  faBook,
+  faMoon,
   faFileAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import NavItem from "../NavItem/index";
 const Header = () => {
+  const bodyClass = document.body.classList;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      bodyClass.replace("light", "dark");
+    } else {
+      bodyClass.replace("dark", "light");
+    }
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <header>
       <NavLink activeClassName="active-nav-link" to="/">
@@ -20,6 +30,16 @@ const Header = () => {
           <NavItem path="/portfolio" label="Portfolio" icon={faFileAlt} />
           <NavItem path="/blog" label="Blog" icon={faCoffee} />
           <NavItem path="/login" label="Login" icon={faSignInAlt} />
+          <li className="nav-item">
+            <FontAwesomeIcon
+              id="theme-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleDarkMode();
+              }}
+              icon={faMoon}
+            />
+          </li>
         </ul>
       </nav>
     </header>
